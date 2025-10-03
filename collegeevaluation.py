5be58b57bc0a82ef17e276c95afffec0efa9e027
kennethMS <kennethwill.ms@gmail.com>
2025-10-02 19:05:44 -0600

    Adding ef else and elif statements

--- collegeevaluation.py
+++ collegeevaluation.py
@@ -11,5 +11,27 @@ if not credits >= 120:
 if not gpa >= 2.0:
   print("Your GPA is not high enough to graduate.")
 
+#code for check not credits and gpa
 if not credits >= 120 and not gpa >= 2.0:
   print("You do not meet either requirement to graduate!")
+
+#code for check credits and gpa
+if (credits >= 120) and (gpa >= 2.0):
+  print("You meet the requirements to graduate!")
+else:
+  print("You do not meet the requirements to graduate.")
+
+
+#code for asign score based on elif use similar to case from java
+grade = 86
+
+if grade >= 90:
+  print("A")
+elif grade >= 80:
+  print("B")
+elif grade >= 70:
+  print("C")
+elif grade >= 60:
+  print("D")
+else:
+  print("F")
