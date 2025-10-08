f4074e0f5162933707fd0cf368d3c1d1121ef9b3
kennethMS <kennethwill.ms@gmail.com>
2025-10-07 21:53:15 -0600

    New code for calc min and show print, variebles updated too

--- salshipproject.py
+++ salshipproject.py
@@ -7,49 +7,58 @@ dronetext = "Drone Shipping - "
 
 #Ground shipping calculation + message to customer
 if weight < 2:
-  price = weight*1.50 + 20.00
+  priceg = weight*1.50 + 20.00
   #a float to concatenate to string need to be converted to str
-  result = groundtext + textprice + str(price)
-  print(result)
+  groundcost = groundtext + textprice + str(priceg)
+  print(groundcost)
 elif weight >= 2 and weight < 6:
-  price = weight*3.00 + 20.00
-  result = groundtext + textprice + str(price)
-  print(result)
+  priceg = weight*3.00 + 20.00
+  groundcost = groundtext + textprice + str(priceg)
+  print(groundcost)
 elif weight >= 6 and weight < 10:
-  price = weight*4.00 + 20.00
-  result = groundtext + textprice + str(price)
-  print(result)
+  priceg = weight*4.00 + 20.00
+  groundcost = groundtext + textprice + str(priceg)
+  print(groundcost)
 elif weight >= 10:
-  price = weight*4.75 + 20.00
-  result = groundtext + textprice + str(price)
-  print(result)
+  priceg = weight*4.75 + 20.00
+  groundcost = groundtext + textprice + str(priceg)
+  print(groundcost)
 else:
-  price == 0
+  priceg == 0
   print("No weight detected")
 
 #Ground shipping premium + message to customer
-price = 125.00
-result = premiumtext + textprice + str(price)
-print(result)
+pricep = 125.00
+premiumcost = premiumtext + textprice + str(pricep)
+print(premiumcost)
 
 #Drone shipping calculation + message to customer
 if weight < 2:
-  price = weight*4.50 + 0.00
+  priced = weight*4.50 + 0.00
   #a float to concatenate to string need to be converted to str
-  result = dronetext + textprice + str(price)
-  print(result)
+  dronecost = dronetext + textprice + str(priced)
+  print(dronecost)
 elif weight >= 2 and weight < 6:
-  price = weight*9.00 + 0.00
-  result = dronetext + textprice + str(price)
-  print(result)
+  priced = weight*9.00 + 0.00
+  dronecost = dronetext + textprice + str(priced)
+  print(dronecost)
 elif weight >= 6 and weight < 10:
-  price = weight*12.00 + 0.00
-  result = dronetext + textprice + str(price)
-  print(result)
+  priced = weight*12.00 + 0.00
+  dronecost = dronetext + textprice + str(priced)
+  print(dronecost)
 elif weight >= 10:
-  price = weight*14.25 + 0.00
-  result = dronetext + textprice + str(price)
-  print(result)
+  priced = weight*14.25 + 0.00
+  dronecost = dronetext + textprice + str(priced)
+  print(dronecost)
 else:
-  price == 0
+  priced == 0
   print("No weight detected")
+
+#Code for check what is the cheapest option, always check identation on python, a syntax error can appear
+cheapest = min(priceg, pricep, priced)
+if cheapest == priceg:
+  print("The cheapest option is " + str(groundcost))
+elif cheapest == pricep:
+    print("The cheapest option is " + str(premiumcost))
+else:
+      print("The cheapest option is " + str(dronecost))
