b42b7a7c8df11679d41f409a3fdb9a0b540823dd
kennethMS <kennethwill.ms@gmail.com>
2025-11-24 20:15:42 -0600

    update of eight mini project

--- functiontripexpense.py
+++ functiontripexpense.py
@@ -17,3 +17,15 @@ print(new_budget_after_shirt)
 
 #calling function and passing argument
 print_remaining_budget(new_budget_after_shirt)
+
+#multiplke returns
+def top_tourist_locations_italy():
+  first = "Rome"
+  second = "Venice"
+  third = "Florence"
+  return first, second, third
+
+most_popular1, most_popular2, most_popular3 = top_tourist_locations_italy()
+print(most_popular1)
+print(most_popular2)
+print(most_popular3)
